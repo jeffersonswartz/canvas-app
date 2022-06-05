5ff74ea0997f16965347412a14ea1e0504d58c39
Jefferson Swartz <jeffersonj@lumel.com>
2022-06-05 22:44:08 +0530

    updated version

--- src/app/user-dialog/user-dialog.component.spec.ts
+++ src/app/user-dialog/user-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UserDialogComponent } from './user-dialog.component';
 
@@ -6,7 +6,7 @@ describe('UserDialogComponent', () => {
   let component: UserDialogComponent;
   let fixture: ComponentFixture<UserDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UserDialogComponent ]
     })
